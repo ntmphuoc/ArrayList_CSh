e9e20c8589ee584df960434d21fdabd56e52d429
My Phuoc <nguyenthimyphuoc2687@gmail.com>
2021-11-29 17:00:26 +0700

    init

---
+++ ArrayList/ArrayList/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+
+namespace ArrayList
+{
+    public class Person
+    {
+        private string name;
+        private int age;
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public int Age
+        {
+            get { return age; }
+            set { age = value; }
+        }
+
+        public Person(string Name, int Age)
+        {
+            this.Name = Name;
+            this.Age = Age;
+        }
+
+        public override string ToString()
+        {
+            return "Name: " +name + "|" + " Age: " + age;
+        }
+    }
+}
